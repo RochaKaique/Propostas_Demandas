c31b20676feaaa41c4bc16b71f5453ba4f560403
Kaique Rocha <kaique.rocha@gmail.com>
2016-05-27 09:46:45 -0300

    segundo

---
+++ src/Novo/Nova.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Novo;
+
+/**
+ *
+ * @author KAIQUE
+ */
+public class Nova {
+    
+    int a = 10;
+    
+}
